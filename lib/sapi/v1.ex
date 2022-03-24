917d673eedd0462f375770f5de93df3c7b2efd4d
vlnic <p4vlow.vl4d@yandex.com>
2022-03-24 10:27:34 +0300

    fix build url and preparing response

--- lib/sapi/v1.ex
+++ lib/sapi/v1.ex
@@ -1,14 +1,14 @@
 defmodule BinanceHttp.SAPI.V1 do
   alias BinanceHttp.Http
 
-  @base_path "/sapi/v1/"
+  @base_path "/sapi/v1"
 
   def capital_detail() do
-    request("capital/config/getall")
+    request("/capital/config/getall")
   end
 
   def system_status() do
-    request("system/status")
+    request("/system/status")
   end
 
   defp request(path) do
