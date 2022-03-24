0088dc5882033e2df647263a1fec6b96f45176ac
vlnic <p4vlow.vl4d@gmail.com>
2022-03-24 13:07:19 +0300

    adding digest

---
+++ lib/Digest.ex
@@ -0,0 +1,22 @@
+defmodule BinanceHttp.Digest do
+  defstruct [:signature, :timestamp]
+
+  def digest(secret_key, params) when is_map(params) do
+    digest(secret_key, glue_params(params))
+  end
+  def digest(secret_key, params) when is_binary(params) do
+    :sha256
+    |> :crypto.hmac(params, secret_key)
+    |> Base.encode16()
+    |> String.downcase()
+  end
+  def digest(_, _), do: {:error, :incorrect_params}
+
+  defp glue_params(%{timestamp: _} = params) when is_map(params) do
+    params
+    |> Map.to_list(params)
+    |> Enum.reduce("", fn({k, v}, result) ->
+      result <> "&k=v"
+    end)
+  end
+end
