1fb6c0a83a4c3a2a451311bc7494aa554e09a5bb
vlnic <p4vlow.vl4d@gmail.com>
2022-04-13 16:14:09 +0300

    delete non actuality docs

--- lib/api/behaviour_def.ex
+++ lib/api/behaviour_def.ex
@@ -1,43 +1,4 @@
 defmodule BinanceHttp.Api.BehaviourDef do
-  @moduledoc """
-  Used to define behaviour module for CcsSdk.API methods, most often it will
-  be used together with Mox.
-
-      # somewhere in ccs_sdk
-      defmodule CcsSdk.API.Trusts.V3 do
-        use CcsSdk.API
-
-        defm :status,
-          method: {:get, "/api/v3/trusts/internal/status"},
-          params: {:primitive, {:array, UUID5}},
-          params_fn: fn(servers) -> %{server_ids: servers} end
-
-      end
-
-      # lib/my_app/business_module.exs
-      defmodule MyApp.BusinessModule do
-        @trusts_v3 Application.compile_env!(:my_app, :trusts_v3_sdk)
-
-        def status(server_ids) do
-          @trusts_v3.status(server_ids)
-        end
-      end
-
-      # config/config.exs
-      config :my_app, :trusts_v3_sdk, CcsSdk.API.Trusts.V3
-
-      # config/test.exs
-      config :my_app, :trusts_v3_sdk, CcsSdk.API.Trusts.V3.Mock
-
-      # test/support/mocks.ex
-      Mox.defmock(CcsSdk.API.Trusts.V3.Mock, for: CcsSdk.API.Trusts.V3.Behaviour)
-
-      # test/my_app/business_module_test.exs
-      Mox.expect(CcsSdk.API.Trusts.V3.Mock, :status, fn(_params) ->
-        {:ok, []}
-      end)
-
-  """
 
   defmacro __using__(_opts \\ []) do
     quote do
