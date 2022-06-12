82dc557428933931c0d63bd9f7183c07d6d6264d
vlnic <p4vlow.vl4d@gmail.com>
2022-06-12 17:51:44 +0300

    added crud methods for order

--- lib/api/v3/v3.ex
+++ lib/api/v3/v3.ex
@@ -29,4 +29,67 @@ defmodule BinanceHttp.Api.V3 do
     params: [
       symbol: {:string, default: nil}
     ]
+
+  action :new_order,
+    endpoint: {:post, "/api/v3/order"},
+    auth_type: :trade,
+    params: [
+      symbol: :string,
+      side: :string,
+      type: :string,
+      timeInForce: {:string, default: nil},
+      quantity: {:float, default: nil},
+      quoteOrderQty: {:float, default: nil},
+      price: {:float, default: nil},
+      newClientOrderId: {:string, default: nil},
+      stopPrice: {:float, default: nil},
+      trailingDelta: {:float, default: nil},
+      icebergQty: {:float, default: nil},
+      newOrderRespType: {:string, default: "FULL"},
+      recvWindow: {:integer, default: nil}
+    ]
+
+  action :cancel_order,
+    endpoint: {:delete, "/api/v3/order"},
+    auth_type: :trade,
+    params: [
+      symbol: :string,
+      orderId: {:integer, default: nil},
+      origClientOrderId: {:string, default: nil},
+      newClientOrderId: {:string, default: nil}
+    ]
+
+  action :cancel_all_orders,
+    endpoint: {:delete, "/api/v3/openOrders"},
+    auth_type: :trade,
+    params: [
+      symbol: :string
+    ]
+
+  action :query_order,
+    endpoint: {:get, "/api/v3/order"},
+    auth_type: :user_data,
+    params: [
+      symbol: :string,
+      orderId: {:integer, default: nil},
+      origClientOrderId: {:string, default: nil}
+    ]
+
+  action :open_orders,
+    endpoint: {:get, "/api/v3/openOrders"},
+    auth_type: :user_data,
+    params: [
+      symbol: {:string, default: nil}
+    ]
+
+  action :all_orders,
+    endpoint: {:get, "/api/v3/allOrders"},
+    auth_type: :user_data,
+    params: [
+      symbol: :string,
+      orderId: {:integer, default: nil},
+      startTime: {:integer, default: nil},
+      endTime: {:integer, default: nil},
+      limit: {:integer, default: nil}
+    ]
 end
