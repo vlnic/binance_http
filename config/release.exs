856ded12a22de0e7843ebd75d6f6472898274460
vlnic <p4vlow.vl4d@gmail.com>
2022-07-06 12:31:49 +0300

    clear unused configs

--- config/release.exs
+++
@@ -1,5 +0,0 @@
-import Config
-
-config :binance_http,
-       secret_key: "BwKaFZ1ch8vsse9qayvN3CgSm7rCN9fywDCWaR2XGhKCuH86wnPTvEk0dxjzuiuU",
-       api_key: "Q8im4r3SbLfsUdCvpV5Dcw7ItwFGnmXQFd3jCiIuZUsrDGFl7LNFU96yqMBR4ikg"
